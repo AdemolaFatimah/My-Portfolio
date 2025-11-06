63f4fb3f1340cc3fd2edc60ef1c80096ac4daa5b
Fatimah <fatimah.ademola@yahoo.com>
2025-11-06 16:19:29 +0100

    Worked on the responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Hamburger  
 
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
